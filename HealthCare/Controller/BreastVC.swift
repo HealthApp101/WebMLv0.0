851533d53f9a5455425523ccd29a62c17af9f31a
Pushpinder Pal Singh <thisisme@pushpinderpalsingh.com>
2020-02-01 21:13:06 +0530

    Correct BreastCancer VC

--- HealthCare/Controller/BreastVC.swift
+++ HealthCare/Controller/BreastVC.swift
@@ -18,7 +18,7 @@ class BreastVC: UIViewController{
         super.viewDidLoad()
     }
     func detect(){
-        guard let output = try? BreastCancer.model.prediction(radius_mean: BreastCancer.Variables[0], texture_mean: BreastCancer.Variables[1], perimeter_mean: BreastCancer.Variables[2], area_mean: BreastCancer.Variables[3], smoothness_mean: BreastCancer.Variables[4], compactness_mean: BreastCancer.Variables[5], concavity_mean: BreastCancer.Variables[6], concave_points_mean: BreastCancer.Variables[7], symmetry_mean: BreastCancer.Variables[8], fractal_dimension_mean: BreastCancer.Variables[9], radius_se: BreastCancer.Variables[10], texture_se: BreastCancer.Variables[11], perimeter_se: BreastCancer.Variables[12], area_se: BreastCancer.Variables[13], smoothness_se: BreastCancer.Variables[14], compactness_se: BreastCancer.Variables[15], concavity_se: BreastCancer.Variables[16], concave_points_se: BreastCancer.Variables[17], symmetry_se: BreastCancer.Variables[19], fractal_dimension_se: BreastCancer.Variables[20], radius_worst: BreastCancer.Variables[21], texture_worst: BreastCancer.Variables[22], perimeter_worst: BreastCancer.Variables[23], area_worst: BreastCancer.Variables[24], smoothness_worst: BreastCancer.Variables[25], compactness_worst: BreastCancer.Variables[26], concavity_worst: BreastCancer.Variables[27], concave_points_worst: BreastCancer.Variables[28], symmetry_worst: BreastCancer.Variables[29], fractal_dimension_worst: BreastCancer.Variables[30]) else{
+        guard let output = try? BreastCancer.model.prediction(radius_mean: BreastCancer.Variables[0], texture_mean: BreastCancer.Variables[1], perimeter_mean: BreastCancer.Variables[2], area_mean: BreastCancer.Variables[3], smoothness_mean: BreastCancer.Variables[4], compactness_mean: BreastCancer.Variables[5], concavity_mean: BreastCancer.Variables[6], concave_points_mean: BreastCancer.Variables[7], symmetry_mean: BreastCancer.Variables[8], fractal_dimension_mean: BreastCancer.Variables[9], radius_se: BreastCancer.Variables[10], texture_se: BreastCancer.Variables[11], perimeter_se: BreastCancer.Variables[12], area_se: BreastCancer.Variables[13], smoothness_se: BreastCancer.Variables[14], compactness_se: BreastCancer.Variables[15], concavity_se: BreastCancer.Variables[16], concave_points_se: BreastCancer.Variables[17], symmetry_se: BreastCancer.Variables[18], fractal_dimension_se: BreastCancer.Variables[19], radius_worst: BreastCancer.Variables[20], texture_worst: BreastCancer.Variables[21], perimeter_worst: BreastCancer.Variables[22], area_worst: BreastCancer.Variables[23], smoothness_worst: BreastCancer.Variables[24], compactness_worst: BreastCancer.Variables[25], concavity_worst: BreastCancer.Variables[26], concave_points_worst: BreastCancer.Variables[27], symmetry_worst: BreastCancer.Variables[28], fractal_dimension_worst: BreastCancer.Variables[29]) else{
             fatalError("Input Failed")
         }
         let result = output.target
@@ -33,7 +33,8 @@ class BreastVC: UIViewController{
             
             count = count+1
         }
-        detect()
+
+       detect()
         for textfields in BreastTextField {
             textfields.text = " "
         }
--- HealthCare/Controller/LiverVC.swift
+++ HealthCare/Controller/LiverVC.swift
@@ -11,6 +11,7 @@ import CoreML
 
 class LiverVC : UIViewController, UINavigationControllerDelegate{
 
+
     @IBOutlet var LiverTextField: [UITextField]!
     
     override func viewDidLoad() {
@@ -19,7 +20,7 @@ class LiverVC : UIViewController, UINavigationControllerDelegate{
     
     func detect(){
         
-        guard let output = try? Liver.model.prediction(Age: Liver.Variables[0], Gender: Liver.Variables[1], Total_Bilirubin: Liver.Variables[2], Direct_Bilirubin: Liver.Variables[3], Alkaline_Phosphotase: Liver.Variables[4], Alamine_Aminotransferase: Liver.Variables[4], Aspartate_Aminotransferase: Liver.Variables[5], Total_Protiens: Liver.Variables[6], Albumin: Liver.Variables[7], Albumin_and_Globulin_Ratio: Liver.Variables[8]) else{
+        guard let output = try? Liver.model.prediction(Age: Liver.Variables[0], Gender: Liver.Variables[1], Total_Bilirubin: Liver.Variables[2], Direct_Bilirubin: Liver.Variables[3], Alkaline_Phosphotase: Liver.Variables[4], Alamine_Aminotransferase: Liver.Variables[5], Aspartate_Aminotransferase: Liver.Variables[6], Total_Protiens: Liver.Variables[7], Albumin: Liver.Variables[8], Albumin_and_Globulin_Ratio: Liver.Variables[9]) else{
             fatalError()
         }
        
@@ -35,7 +36,13 @@ class LiverVC : UIViewController, UINavigationControllerDelegate{
             
             count = count+1
         }
-        detect()
+         count = 0
+        for a in Liver.Variables{
+            print(a)
+            print(count)
+            count = count + 1
+        }
+       // detect()
         for textfields in LiverTextField {
             textfields.text = " "
         }
